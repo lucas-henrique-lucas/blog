0dc1b3813c122e058af5a9fe99adb750cea2be0b
Lucas <159969857+lucas-henrique-lucas@users.noreply.github.com>
2025-08-06 12:08:17 -0300

    Add files via upload

---
+++ eleventy.config.mjs
@@ -0,0 +1,160 @@
+import fs from "fs";
+import path from "path";
+import postcss from "postcss";
+import tailwindcss from "@tailwindcss/postcss";
+
+import eleventyPluginFilesMinifier from "@sherby/eleventy-plugin-files-minifier";
+import lazyImagesPlugin from "eleventy-plugin-lazyimages";
+
+if (process.env.NODE_ENV?.toLocaleLowerCase() !== "production") {
+  import("dotenv").then((dotenv) => dotenv.config());
+}
+
+export default function (eleventyConfig) {
+  // minifier js
+  import("./scripts/minify.js")
+    .then((minifyModule) => {
+      eleventyConfig.addTransform(
+        "minify",
+        minifyModule.default || minifyModule
+      );
+    })
+    .catch((error) =>
+      console.error("Error importing minify transform:", error)
+    );
+
+  // lazyImages and Files Minifier
+  eleventyConfig.addPlugin(lazyImagesPlugin, {
+    appendInitScript: false, // disable cdn script
+  });
+  if (process.env.NODE_ENV?.toLowerCase() === "production") {
+    eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+  }
+
+  // Global data for root
+  eleventyConfig.addGlobalData("lang", "pt-BR"); // your lang attribute
+  eleventyConfig.addGlobalData("rootTitle", "Lucas/perfil.");
+  eleventyConfig.addGlobalData("rootURL", "https://lucs.netlify.app");
+  eleventyConfig.addGlobalData(
+    "quotes",
+    "<i>Opa, esse é um template feito por mim para hospedar suas imagens.</i><br /><b>― Lucas 👋🏻🐸</b>"
+  );
+  eleventyConfig.addGlobalData("SUPABASE_URL", process.env.SUPABASE_URL);
+  eleventyConfig.addGlobalData("SUPABASE_KEY", process.env.SUPABASE_KEY);
+
+  // Bypass dir
+  const passthroughCopies = [
+    "src/robots.txt",
+    "src/asset/",
+    "src/CNAME",
+    "admin",
+    "public",
+  ];
+  passthroughCopies.forEach((path) => eleventyConfig.addPassthroughCopy(path));
+
+  // Custom collection
+  const collectionConfigs = [
+    { name: "posts", glob: "src/blog/**/*.md" },
+    { name: "photos", glob: "src/photos/**/*.md" },
+    { name: "recentPosts", glob: "src/blog/*.md", limit: 3 },
+    { name: "recentPhotos", glob: "src/photos/*.md", limit: 6 },
+  ];
+
+  // Loop custom collection
+  collectionConfigs.forEach((config) => {
+    eleventyConfig.addCollection(config.name, function (collectionApi) {
+      let items = collectionApi.getFilteredByGlob(config.glob);
+      return config.limit ? items.reverse().slice(0, config.limit) : items;
+    });
+  });
+
+  // Filter to get first image array cover
+  eleventyConfig.addFilter("firstCoverImage", function (cover) {
+    if (!cover) {
+      return ""; // Retorna uma string vazia se 'cover' for nulo ou indefinido
+    }
+
+    // Caso 1: 'cover' é uma lista de objetos (como nos posts de fotos)
+    if (
+      Array.isArray(cover) &&
+      cover.length > 0 &&
+      typeof cover[0] === "object" &&
+      cover[0].url
+    ) {
+      return cover[0].url;
+    }
+
+    // Caso 2: 'cover' é uma string simples (como nos posts de blog)
+    if (typeof cover === "string") {
+      return cover;
+    }
+
+    // Caso 3: 'cover' é um objeto único com uma propriedade 'url'
+    if (typeof cover === "object" && cover.url) {
+      return cover.url;
+    }
+
+    // Retorna uma string vazia se nenhum dos casos acima for verdadeiro
+    return "";
+  });
+
+  // Tags collection for blog and photos
+  ["blog", "photos"].forEach((type) => {
+    eleventyConfig.addCollection(`${type}Tags`, getTags(type));
+  });
+
+  // Filter to limit words string
+  eleventyConfig.addLiquidFilter("limitWords", function (str, wordLimit) {
+    let words = str.split(" ");
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(" ") + "...";
+    }
+    return str;
+  });
+
+  /**
+   * Get tag from collection
+   * @param {'blog'|'photos'} type - Collection type (blog or photos)
+   * @returns {Array} - Array uniq tags
+   */
+  function getTags(type) {
+    return (collection) => {
+      let tagsSet = new Set();
+      collection.getAll().forEach((item) => {
+        if (item.filePathStem.includes(`/${type}/`) && item.data.tags) {
+          item.data.tags.forEach((tag) => {
+            tagsSet.add(tag);
+          });
+        }
+      });
+      return Array.from(tagsSet);
+    };
+  }
+
+  eleventyConfig.on("eleventy.before", async () => {
+    const tailwindInputPath = path.resolve("./src/styles.css");
+    const tailwindOutputPath = "./_site/asset/css/style.css";
+    const cssContent = fs.readFileSync(tailwindInputPath, "utf8");
+    const outputDir = path.dirname(tailwindOutputPath);
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await postcss([tailwindcss()]).process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+}
